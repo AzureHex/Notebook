f734bd48d434476ba79e9c8ae0a63674174c6a21
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-10-21 21:03:43 +0530

    Updated Pwsh Scripts

--- .scripts/pwsh.ps1
+++ .scripts/pwsh.ps1
@@ -6,12 +6,19 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 # Add the extras bucket
 scoop bucket add extras
+scoop bucket add nerd-fonts
 
 # Install the specified applications from Scoop
-scoop install 7zip adb ani-cli aria2 bat btop cacert curl eza ffmpeg fnm fzf gcc git googlechrome grep lua-language-server llvm localsend mingw mpv nano neovim obsidian ollama openssl raspberry-pi-imager sed starship tailscale tgpt tldr wezterm wget whatsapp winfetch yazi yt-dlp zoxide
+scoop install 7zip adb ani-cli aria2 bat btop cacert curl ffmpeg fnm fzf gcc git googlechrome grep lua-language-server llvm localsend mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager sed starship tailscale tree tgpt tldr wezterm wget whatsapp winfetch yazi yt-dlp zoxide
 
 # Install the specified applications using Winget
 winget install Mozilla.Thunderbird MacPaw.Encrypto AdrienAllard.FileConverter FxSoundLLC.FxSound Flywheel.Local Microsoft.PowerToys KamilSzymborski.WindowCenteringHelper Debian.Debian
+
+# Install the Terminal-Icons Module
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+# Install the PSReadLine Module
+Install-Module -Name PSReadLine -Scope CurrentUser
 
 # Install the PSWindowsUpdate module
 Install-Module PSWindowsUpdate -Force
