c62f412d05c58d3819a67b5d4dee62537f0af6eb
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-10-23 11:16:28 +0530

    Updated pwsh profile

--- .scripts/pwsh.ps1
+++ .scripts/pwsh.ps1
@@ -9,7 +9,7 @@ scoop bucket add extras
 scoop bucket add nerd-fonts
 
 # Install the specified applications from Scoop
-scoop install 7zip adb ani-cli aria2 bat btop cacert chafa curl fd ffmpeg file fnm fzf gcc git grep googlechrome grep imagemagick innounp lazygit less llvm localsend lua-language-server mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager ripgrep sed starship tailscale tree tgpt tldr vcredist2022 wezterm wget whatsapp winfetch yazi yt-dlp zoxide
+scoop install 7zip adb ani-cli aria2 bat btop cacert chafa curl eza fd ffmpeg file fnm fzf gcc git grep googlechrome grep imagemagick innounp lazygit less llvm localsend lua-language-server mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager ripgrep sed starship tailscale tree tgpt tldr vcredist2022 wezterm wget whatsapp winfetch yazi yt-dlp zoxide
 
 # Install the specified applications using Winget
 winget install Mozilla.Thunderbird MacPaw.Encrypto AdrienAllard.FileConverter FxSoundLLC.FxSound Flywheel.Local Microsoft.PowerToys KamilSzymborski.WindowCenteringHelper Debian.Debian
