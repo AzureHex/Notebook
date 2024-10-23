c6c57f436c46f6af59494f7f0a5dd721cf9a3324
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-10-23 13:33:35 +0530

    Updated pwsh.ps1

--- .scripts/pwsh.ps1
+++ .scripts/pwsh.ps1
@@ -7,10 +7,10 @@ scoop bucket add extras
 scoop bucket add nerd-fonts
 
 # Install the specified applications from Scoop
-scoop install 7zip adb ani-cli aria2 bat btop cacert chafa curl eza fd ffmpeg file fnm fzf gcc git grep googlechrome grep imagemagick innounp lazygit less llvm localsend lua-language-server mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager ripgrep sed starship tailscale tree tgpt tldr vcredist2022 wezterm wget whatsapp winfetch yazi yt-dlp zoxide
+scoop install 7zip adb ani-cli aria2 bat btop cacert chafa curl eza fd ffmpeg file fnm fzf gcc git grep googlechrome grep imagemagick innounp lazygit less llvm localsend lua-language-server mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager ripgrep sed starship tailscale tree tgpt tldr vcredist2022 wezterm wget winfetch yazi yt-dlp zoxide
 
 # Install the specified applications using Winget
-winget install Mozilla.Thunderbird MacPaw.Encrypto AdrienAllard.FileConverter FxSoundLLC.FxSound Flywheel.Local Microsoft.PowerToys KamilSzymborski.WindowCenteringHelper Debian.Debian
+winget install Mozilla.Thunderbird MacPaw.Encrypto AdrienAllard.FileConverter FxSoundLLC.FxSound Flywheel.Local Microsoft.PowerToys KamilSzymborski.WindowCenteringHelper Canonical.Ubuntu Debian.Debian 9NKSQGP7F2NH
 
 # Install the Terminal-Icons Module
 Install-Module -Name Terminal-Icons -Repository PSGallery
