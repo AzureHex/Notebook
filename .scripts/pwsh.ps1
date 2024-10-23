159f4dca440087d86d49425d1a7271f136be8b22
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-10-23 14:42:05 +0530

    Updated pwsh.ps1 to install nerd fonts

--- .scripts/pwsh.ps1
+++ .scripts/pwsh.ps1
@@ -5,6 +5,7 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 # Add the extras bucket
 scoop bucket add extras
 scoop bucket add nerd-fonts
+scoop install nerd-fonts/JetBrains-Mono
 
 # Install the specified applications from Scoop
 scoop install 7zip adb ani-cli aria2 bat btop cacert chafa curl eza fd ffmpeg file fnm fzf gcc git grep googlechrome grep imagemagick innounp lazygit less llvm localsend lua-language-server mingw mpv msys2 nano neovim nu obsidian oh-my-posh ollama openssl raspberry-pi-imager ripgrep sed starship tailscale tree tgpt tldr vcredist2022 wezterm wget winfetch yazi yt-dlp zoxide
