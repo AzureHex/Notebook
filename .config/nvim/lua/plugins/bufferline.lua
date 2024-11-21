7240f99df4f9b12df154a420d833116fc5bf749b
AzureHex <183492338+AzureHex@users.noreply.github.com>
2024-11-22 02:44:04 +0530

    Added bufferline plugin

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+    "akinsho/bufferline.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        require("bufferline").setup({
+        })
+
+    vim.opt.termguicolors = true
+    end
+}
