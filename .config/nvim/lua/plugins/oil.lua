44cfcd255f15eabd4eacb574b39c4b90f7cd2944
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-12-02 18:26:41 +0530

    Added show_hidden function for oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,9 @@ return {
                 max_width = 100,
                 max_height = 40,
             },
+            view_options = {
+                show_hidden = true,
+            },
         })
         vim.keymap.set("n", "-", oil.toggle_float, {})
     end,
